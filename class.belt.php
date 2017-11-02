e72ef0d9fb5a395217fce659f009ad800969cd61
Tomek <tomek@tusonic.pl>
2017-11-02 23:30:32 +0100

    0.1

---
+++ class.belt.php
@@ -0,0 +1,121 @@
+<?php ob_start();
+session_start();
+
+
+class Belt extends Database {
+
+
+    public function viewBelt()
+    {
+        $viewBelt = $this->pdo->prepare('select * from belt');
+        $viewBelt->execute();
+
+        echo' 
+ 
+            <table class="table">
+                 <thead>
+                       <tr> 
+                             <th scope="col">#</th>
+                             <th scope="col">Length</th>
+                             <th scope="col">Width</th>
+                             <th scope="col">Typee</th>
+                        </tr>
+                   </thead>
+                <tbody>
+                   
+  
+         ';
+
+        while ($row = $viewBelt->fetch(PDO::FETCH_ASSOC))
+        {
+            $id = $row['id'];
+            $length = $row['length'];
+            $width = $row['width'];
+            $type = $row['type'];
+
+            echo'
+                <tr>
+                    <th scope="row">'.$id.'</th>
+                    <td>'.$length.'</td>
+                    <td>'.$width.'</td>
+                    <td>'.$type.'</td>
+                </tr>
+            ';
+
+        }
+
+        echo'
+        
+                  </tbody>
+               </table>
+        
+         ';
+
+    }
+
+
+    public function confBelt()
+    {
+        $viewBelt = $this->pdo->prepare('select * from belt');
+        $viewBelt->execute();
+
+        echo' 
+ 
+            <table class="table">
+                 <thead>
+                       <tr> 
+                             <th scope="col">#</th>
+                             <th scope="col">Length</th>
+                             <th scope="col">Width</th>
+                             <th scope="col">Typee</th>
+                             <th scope="col">Edit</th>
+                             <th scope="col">Delete</th>
+                        </tr>
+                   </thead>
+                <tbody>
+                   
+  
+         ';
+
+        while ($row = $viewBelt->fetch(PDO::FETCH_ASSOC))
+        {
+            $id = $row['id'];
+            $length = $row['length'];
+            $width = $row['width'];
+            $type = $row['type'];
+
+
+
+            echo'
+                <tr>
+                    <th scope="row">'.$id.'</th>
+                    <td>'.$length.'</td>
+                    <td>'.$width.'</td>
+                    <td>'.$type.'</td>
+                    <td><a href="deleteBelt.php?id='.$row['id'].'">DeleteGET</a></td>
+                    <td><form method="POST" action="deleteBelt.php"><input type="hidden" value="'.$row['id'].'" name="id"/><input type="submit" value="Remove"/></form></td>
+                </tr>
+            ';
+
+        }
+
+        echo'
+        
+                  </tbody>
+               </table>
+        
+         ';
+
+
+
+    }
+
+
+
+
+}
+
+
+
+ob_end_flush();
+?>
---
+++ deleteBelt.php
@@ -0,0 +1,21 @@
+<?php
+
+ob_start();
+session_start();
+
+echo '</br> get';
+$get1 = $_GET['id'];
+echo $get1;
+
+
+echo '</br> post';
+$post1 = $_POST['id'];
+echo $post1;
+
+
+ob_end_flush();
+//session_destroy();
+?>
+
+
+
