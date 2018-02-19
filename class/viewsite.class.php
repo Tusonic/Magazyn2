1b1abeee184b91caaf7666c77a4190d7d0f1ad73
Tusonic <tomek@tusonic.pl>
2018-02-19 23:38:23 +0100

    0.5.5

--- class/viewsite.class.php
+++ class/viewsite.class.php
@@ -33,7 +33,73 @@ class viewsite {
 
         }
 
-     public function endhead()
+    public function menu()
+    {
+       echo '
+       
+       <p></p>
+        <p class="text-center">Magazyn 2</p>
+        <p></p>
+
+   <div class="row">
+    <div class="col-md-4">
+        <h2><p class="text-center">TRANSACTION</p></h2>
+        <p></p>
+        <p><a class="btn btn-primary btn-lg btn-block" href="transaction/view.php" role="button">View &raquo;</a></p>
+        <p><a class="btn btn-primary btn-lg btn-block" href="transaction/add.php" role="button">Add &raquo;</a></p>
+        <p><a class="btn btn-primary btn-lg btn-block" href="transaction/edit.php" role="button">Edit &raquo;</a></p>
+        <p><a class="btn btn-primary btn-lg btn-block" href="transaction/delete.php" role="button">Delete &raquo;</a></p>
+    </div>
+    <div class="col-md-4">
+    </div>
+    <div class="col-md-4">
+        <h2><p class="text-center">ACCOUNT</p></h2>
+        <p><a class="btn btn-primary btn-lg btn-block" href="#" role="button">LOGOUT &raquo;</a></p>
+        <p>INFO</p>
+        <p>INFO</p>
+        <p>INFO</p>
+    </div>
+
+
+</div>
+
+<div class="row">
+    <div class="col-md-4">
+        <h2><p class="text-center">BELT</p></h2>
+        <p></p>
+        <p><a class="btn btn-primary btn-lg btn-block" href="belt/view.php" role="button">View &raquo;</a></p>
+        <p><a class="btn btn-primary btn-lg btn-block" href="belt/add.php" role="button">Add &raquo;</a></p>
+        <p><a class="btn btn-primary btn-lg btn-block" href="belt/edit.php" role="button">Edit &raquo;</a></p>
+        <p><a class="btn btn-primary btn-lg btn-block" href="belt/delete.php" role="button">Delete &raquo;</a></p>
+    </div>
+    <div class="col-md-4">
+        <h2><p class="text-center">CLIENT</p></h2>
+        <p></p>
+        <p><a class="btn btn-primary btn-lg btn-block" href="client/view.php" role="button">View &raquo;</a></p>
+        <p><a class="btn btn-primary btn-lg btn-block" href="client/add.php" role="button">Add &raquo;</a></p>
+        <p><a class="btn btn-primary btn-lg btn-block" href="client/edit.php" role="button">Edit &raquo;</a></p>
+        <p><a class="btn btn-primary btn-lg btn-block" href="client/delete.php" role="button">Delete &raquo;</a></p>
+    </div>
+    <div class="col-md-4">
+        <h2><p class="text-center">USER</p></h2>
+        <p></p>
+        <p><a class="btn btn-primary btn-lg btn-block" href="user/view.php" role="button">View &raquo;</a></p>
+        <p><a class="btn btn-primary btn-lg btn-block" href="user/add.php" role="button">Add &raquo;</a></p>
+        <p><a class="btn btn-primary btn-lg btn-block" href="user/edit.php" role="button">Edit &raquo;</a></p>
+        <p><a class="btn btn-primary btn-lg btn-block" href="user/delete.php" role="button">Delete &raquo;</a></p>
+    </div>
+
+</div>
+       
+       
+       ';
+    }
+
+    public function loginin() {
+
+    }
+
+    public function endhead()
         {
 
             echo'
--- index.php
+++ index.php
@@ -7,64 +7,36 @@ session_start();
 
 $site = new viewsite();
 $site->starthead();
+$site->menu();
+
+
 
 ?>
 
+// form
 
-<p></p>
-<p class="text-center">Magazyn 2</p>
-<p></p>
-
-<div class="row">
-    <div class="col-md-4">
-        <h2><p class="text-center">TRANSACTION</p></h2>
-        <p></p>
-        <p><a class="btn btn-primary btn-lg btn-block" href="transaction/view.php" role="button">View &raquo;</a></p>
-        <p><a class="btn btn-primary btn-lg btn-block" href="transaction/add.php" role="button">Add &raquo;</a></p>
-        <p><a class="btn btn-primary btn-lg btn-block" href="transaction/edit.php" role="button">Edit &raquo;</a></p>
-        <p><a class="btn btn-primary btn-lg btn-block" href="transaction/delete.php" role="button">Delete &raquo;</a></p>
+<div class="col-md-12">
+    <div class="modal-dialog" style="margin-bottom:0">
+        <div class="modal-content">
+            <div class="panel-heading">
+                <h3 class="panel-title">LOGIN</h3>
+            </div>
+            <div class="panel-body">
+                <form role="form">
+                    <fieldset>
+                        <div class="form-group">
+                            <input class="form-control" placeholder="Login" name="email" type="login" value"">
+                        </div>
+                        <div class="form-group">
+                            <input class="form-control" placeholder="Password" name="password" type="password" value="">
+                        </div>
+                        <a href="#" class="btn btn-sm btn-success">Login</a>
+                    </fieldset>
+                </form>
+            </div>
+        </div>
     </div>
-    <div class="col-md-4">
-    </div>
-    <div class="col-md-4">
-        <h2><p class="text-center">ACCOUNT</p></h2>
-        <p><a class="btn btn-primary btn-lg btn-block" href="#" role="button">LOGOUT &raquo;</a></p>
-        <p>INFO</p>
-        <p>INFO</p>
-        <p>INFO</p>
-    </div>
-
-
 </div>
-
-<div class="row">
-    <div class="col-md-4">
-        <h2><p class="text-center">BELT</p></h2>
-        <p></p>
-        <p><a class="btn btn-primary btn-lg btn-block" href="belt/view.php" role="button">View &raquo;</a></p>
-        <p><a class="btn btn-primary btn-lg btn-block" href="belt/add.php" role="button">Add &raquo;</a></p>
-        <p><a class="btn btn-primary btn-lg btn-block" href="belt/edit.php" role="button">Edit &raquo;</a></p>
-        <p><a class="btn btn-primary btn-lg btn-block" href="belt/delete.php" role="button">Delete &raquo;</a></p>
-    </div>
-    <div class="col-md-4">
-        <h2><p class="text-center">CLIENT</p></h2>
-        <p></p>
-        <p><a class="btn btn-primary btn-lg btn-block" href="client/view.php" role="button">View &raquo;</a></p>
-        <p><a class="btn btn-primary btn-lg btn-block" href="client/add.php" role="button">Add &raquo;</a></p>
-        <p><a class="btn btn-primary btn-lg btn-block" href="client/edit.php" role="button">Edit &raquo;</a></p>
-        <p><a class="btn btn-primary btn-lg btn-block" href="client/delete.php" role="button">Delete &raquo;</a></p>
-    </div>
-    <div class="col-md-4">
-        <h2><p class="text-center">USER</p></h2>
-        <p></p>
-        <p><a class="btn btn-primary btn-lg btn-block" href="user/view.php" role="button">View &raquo;</a></p>
-        <p><a class="btn btn-primary btn-lg btn-block" href="user/add.php" role="button">Add &raquo;</a></p>
-        <p><a class="btn btn-primary btn-lg btn-block" href="user/edit.php" role="button">Edit &raquo;</a></p>
-        <p><a class="btn btn-primary btn-lg btn-block" href="user/delete.php" role="button">Delete &raquo;</a></p>
-    </div>
-
-</div>
-
 
 
 <?php
